002400264db1a2a23715a477bb626a36d54096d8
sydneyfullstack <samuelju2017@gmail.com>
2023-04-05 08:44:03 +1000

    AON consider multiple match in main arr for first num in sub arr

--- src/main/java/com/samuel/AON/TaskTwoFindArrayIndex.java
+++ src/main/java/com/samuel/AON/TaskTwoFindArrayIndex.java
@@ -3,10 +3,7 @@ package com.samuel.AON;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class TaskTwoFindArray {
-    public static void main(String[] args) {
-
-    }
+public class TaskTwoFindArrayIndex {
 
     @Test
     public void testArraysShouldEqualTo2() {
@@ -19,17 +16,22 @@ public class TaskTwoFindArray {
         if(arr1 == null || arr2 == null || arr1.length == 0 || arr2.length == 0) return -1;
         if(arr1.length < arr2.length ) return -1;
 
+        int index = -1;
+        int indexTemp;
         for (int i = 0; i < arr1.length; i++) {
             if(arr1[i] == arr2[0]){
-                int index = i;
+                index = i;
+                indexTemp = i;
                 if(arr2.length > arr1.length - index) return -1;
 
                 for (int j : arr2) {
-                    if (arr1[i++] != j) return -1;
+                    if (arr1[indexTemp++] != j) {
+                        index = -1;
+                        break;
+                    };
                 }
-                return index;
             }
         }
-        return -1;
+        return index;
     }
 }
